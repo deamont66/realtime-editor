152ad48d6400deb54f0d2feacb46c806b91fe3b2
Jiří Šimeček <simecekjiri95@gmail.com>
2018-03-28 17:09:51 +0200

    Add 404 styles

--- app/client/src/Pages/Errors/Error404.js
+++ app/client/src/Pages/Errors/Error404.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import {translate} from 'react-i18next';
+import withStyles from 'material-ui/styles/withStyles';
+
 import Typography from 'material-ui/Typography';
+import Paper from 'material-ui/Paper';
+
 import MetaTags from "../../Components/MetaTags";
+
+const styles = theme => ({
+    root: theme.mixins.gutters({
+        paddingTop: theme.typography.pxToRem(16),
+        paddingBottom: theme.typography.pxToRem(16),
+        marginTop: theme.typography.pxToRem(theme.spacing.unit * 3),
+        maxWidth: theme.typography.pxToRem(450),
+        marginLeft: 'auto',
+        marginRight: 'auto',
+    }),
+});
 
 const Error404 = (props) => {
     return (
-        <div>
+        <Paper elevation={4} className={props.classes.root}>
             <MetaTags title={props.t('error.not_found')}/>
-            <Typography>{props.t('error.not_found')}</Typography>
-        </div>
+            <Typography align={'center'}>{props.t('error.not_found')}</Typography>
+        </Paper>
     );
 };
 
-export default translate()(Error404);
+export default withStyles(styles)(translate()(Error404));
